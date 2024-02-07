3c5d27e65cbeef606ff79e88660bda8b850cd301
yongwoon kim <yonyon.japan@gmail.com>
2024-02-07 15:26:20 +0900

    Ing Bigger application

--- api/main.py
+++ api/main.py
@@ -1,13 +1,19 @@
 from typing import Union
 from fastapi import FastAPI
 
+from routers import users
+
 app = FastAPI()
+app.include_router(users.router)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
+
+# tutorial: bigger-applications
+# https://fastapi.tiangolo.com/tutorial/bigger-applications/
